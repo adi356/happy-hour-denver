a5a7cf325b679b5b2de6cafaffbbec231b5b3db8
adi356 <75458420+adi356@users.noreply.github.com>
2024-09-25 18:56:38 -0600

    clean up comments in schema

--- backend/src/models/BarSchema.ts
+++ backend/src/models/BarSchema.ts
@@ -1,42 +1,5 @@
-// import mongoose, { Schema, Document } from 'mongoose'
-
-// interface IDailyDeals {
-//     [key: string]: string[];
-// }
-
-// export interface IBar extends Document {
-//     name: string;
-//     image: string;
-//     neighborhood: string;
-//     happy_hour_time: string;
-//     lunch_specials: string[];
-//     happy_hour_deals: string[];
-//     daily_deals: IDailyDeals;
-//     late_night: string[];
-// }
-
-// const BarSchema: Schema = new Schema({
-//     name: { type: String, required: true },
-//     image: { type: URL, required: false },
-//     neighborhood: { type: String, required: true},
-//     happy_hour_time: { type: String, required: false },
-//     lunch_specials: { type: [String], required: false },
-//     happy_hour_deals: { type: [String], required: true},
-//     daily_deals: {
-//         type: Map,
-//         of: [String],
-//         required: false
-//     },
-//     late_night: { type: String, required: false }
-// })
-
-// export default mongoose.model<IBar>('Bar', BarSchema)
-
 import { Schema, model } from 'mongoose'
 
-interface IDailyDeals {
-    [key: string]: string[];
-}
 export interface BarInterface {
     name: string;
     image: string;
