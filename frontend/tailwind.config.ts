c9856a81da0a51ff991c270ae99156177ab93423
adi356 <75458420+adi356@users.noreply.github.com>
2024-09-25 19:38:42 -0600

    changed tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,12 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     './index.html',
+//     './src/**/*.{js,ts,jsx,tsx}'
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
+export default config
